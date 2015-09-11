3a421c5a89a3a98e7894710325762b6dd71b8b5c
Ilya Solovyov <shomyoS@gmail.com>
2015-09-11 21:17:22 +0300

    sync

--- config.h
+++ config.h
@@ -1,63 +1,45 @@
-#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-#define NUMCOLORS 9
-static const char colors[NUMCOLORS][ColLast][9] = {
-	/*  border   foreground  background  */
-	{ "#202020", "#a9a9a9", "#303030" },  /* x01 = darkgray  */
-	{ "#a9a9a9", "#d3d3d3", "#303030" },  /* x02 = lightgray */
-	{ "#202020", "#ff8c00", "#303030" },  /* x03 = orange    */
-	{ "#202020", "#ff0000", "#303030" },  /* x04 = red       */
-	{ "#202020", "#4cbb17", "#303030" },  /* x05 = green     */
-	{ "#202020", "#f4ca16", "#303030" },  /* x06 = yellow    */
-	{ "#202020", "#318ce7", "#303030" },  /* x07 = blue      */
-	{ "#202020", "#cc00cc", "#303030" },  /* x08 = magenta   */
-	{ "#202020", "#00b7eb", "#303030" },  /* x09 = cyan      */
-};
-
-static const char font[]            = "-*-ohsnap.icons-medium-r-normal-*-14-*-*-*-*-*-*-*";
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gap pixel between windows */
-static const unsigned int snap      = 16;       /* snap pixel */
+static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "main", "web", "dev", "media", "temp" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 * WM_CLASS(STRING) = instance, class
-	 * WM_NAME(STRING) = title
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
 	 */
-	/* class          instance    title       tags mask     isfloating   monitor */
-	{ "Chromium",     NULL,       NULL,       1 << 1,       False,       -1 },
-	{ "Electrum",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",      NULL,       NULL,       1 << 4,       False,       -1 },
-	{ "Gimp",         NULL,       NULL,       0,            True,        -1 },
-	{ "Nemo",         NULL,       NULL,       0,            True,        -1 },
-	{ "Pidgin",       NULL,       NULL,       1 << 3,       True,        -1 },
-	{ "Sonata",       NULL,       NULL,       1 << 3,       True,        -1 },
-	{ "Steam",        NULL,       NULL,       0,            True,        -1 },
-	{ "Transmission", NULL,       NULL,       1 << 3,       True,        -1 },
-	{ "Zenity",       NULL,       NULL,       0,            True,        -1 },
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
 };
 
 /* layout(s) */
-static const float mfact      = 0.60; /* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[t]",      tile },    /* first entry is default */
-	{ "[f]",      NULL },    /* no layout function means floating behavior */
-	{ "[m]",      monocle },
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -68,31 +50,20 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[1][ColFG],"-sb", colors[0][ColBG], "-sf", colors[8][ColFG], NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
-static const char *stcmd[]    = { "st", NULL };
-static const char *wwwcmd[]   = { "chromium", NULL };
-static const char *mutecmd[]  = { "amixer", "set", "Master", "toggle", NULL };
-static const char *volupcmd[] = { "amixer", "set", "Master", "5%+", NULL };
-static const char *voldncmd[] = { "amixer", "set", "Master", "5%-", NULL };
-static const char *mpctogglecmd[]  = { "mpc", "toggle", NULL };
-static const char *mpcstopcmd[]    = { "mpc", "stop", NULL };
-static const char *mpcprevcmd[]    = { "mpc", "prev", NULL };
-static const char *mpcnextcmd[]    = { "mpc", "next", NULL };
-static const char *mpcvolupcmd[]   = { "mpc", "volume", "+5", NULL };
-static const char *mpcvoldowncmd[] = { "mpc", "volume", "-5", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { "uxterm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.01} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.01} },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -117,18 +88,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	/* custom */
-	{ MODKEY,                       XK_grave,  spawn,          {.v = stcmd } },
-	{ False,                        XF86XK_HomePage,           spawn,          {.v = wwwcmd } },
-	{ False,                        XF86XK_AudioPlay,          spawn,          {.v = mpctogglecmd } },
-	{ False,                        XF86XK_AudioStop,          spawn,          {.v = mpcstopcmd } },
-	{ False,                        XF86XK_AudioPrev,          spawn,          {.v = mpcprevcmd } },
-	{ False,                        XF86XK_AudioNext,          spawn,          {.v = mpcnextcmd } },
-	{ MODKEY,                       XK_KP_Add,                 spawn,          {.v = mpcvolupcmd } },
-	{ MODKEY,                       XK_KP_Subtract,            spawn,          {.v = mpcvoldowncmd  } },
-	{ False,                        XF86XK_AudioMute,          spawn,          {.v = mutecmd  } },
-	{ False,                        XF86XK_AudioRaiseVolume,   spawn,          {.v = volupcmd  } },
-	{ False,                        XF86XK_AudioLowerVolume,   spawn,          {.v = voldncmd  } },
 };
 
 /* button definitions */
