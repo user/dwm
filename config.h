6308c3e2b85c8eeee59f83a3185d6a1703ab0c9f
Ilya Solovyov <shomyoS@gmail.com>
2015-09-15 06:36:39 +0300

    I know what I am doing. Trust me.

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static const char *fonts[] = {
 };
 static const char dmenufont[] = "ohsnap.icons:size=14";
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gap pixel between windows */
+static const unsigned int gappx     = 32;        /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
