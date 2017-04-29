1a18e5e714c1e3b84974ac23d213d5c4d5239f49
Ilya Solovyov <shomyoS@gmail.com>
2017-04-29 20:06:02 +0300

    sync

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 6.1
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /usr
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/xorg
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
