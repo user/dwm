764fdb8157d9f1060a3a1da882d84f21273f0e7e
Ilya Solovyov <shomyoS@gmail.com>
2017-05-07 11:26:14 +0300

    add tilegap

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
